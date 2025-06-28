71681294a53c7f11658ff0c6b63204af2beaa1e9
radwan <eslamradwan145@gmail.com>
2025-06-28 07:28:11 +0300

    feat: implement pagination for popular and top-rated movies, and add test files for movie controller and movie services

--- services/movieService.js
+++ services/movieService.js
@@ -6,19 +6,11 @@ dotenv.config()
 const API_BASE_URL = process.env.API_BASE_URL
 const API_KEY = process.env.API_KEY
 
-const getPopularMovies = async () => {
-    try {
-        const response = await axios.get(`${API_BASE_URL}/movie/popular`, {
-            headers: { 'Authorization': `Bearer ${API_KEY}` }
-        })
-        return response.data;
-    } catch (error) {
-        throw new Error(error.response?.data?.status_message || error.message)
-    }
-}
-const getTopRatedMovies = async () => {
-    try {
-        const response = await axios.get(`${API_BASE_URL}/movie/top_rated`, {
+const fecthNumberOfPages = async (domain, page) => {
+
+     try {
+        
+        const response = await axios.get(`${API_BASE_URL}/movie/${domain}?page=${page}`, {
             headers: { 'Authorization': `Bearer ${API_KEY}` }
         })
         return response.data;
@@ -27,15 +19,33 @@ const getTopRatedMovies = async () => {
     }
 }
 
-const getMovieDetail = async (id) => {
-    try {
-        const response = await axios.get(`${API_BASE_URL}/movie/${id}`, {
-            headers: { 'Authorization': `Bearer ${API_KEY}` }
-        })
-        return response.data;
-    } catch (error) {
-        throw new Error(error.response?.data?.status_message || error.message)
+const getPopularMovies = async (count) => {
+    const arr = []
+
+    for (let i = 0; i < Math.ceil(count / 20.0);i++)
+    {
+        arr.push(fecthNumberOfPages('popular', i))
     }
+    return arr;
+}
+const getTopRatedMovies = async () => {
+    const arr = []
+
+    for (let i = 0; i < Math.ceil(count / 20.0);i++)
+    {
+        arr.push(fecthNumberOfPages('top_rated', i))
+    }
+    return arr;
+}
+
+const getMovieDetail = async (id) => {
+     const arr = []
+
+    for (let i = 0; i < Math.ceil(count / 20.0);i++)
+    {
+        arr.push(fecthNumberOfPages(id, i))
+    }
+    return arr;
 }
 
 module.exports = { getPopularMovies, getTopRatedMovies, getMovieDetail }
